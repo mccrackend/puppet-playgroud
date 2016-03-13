acd9a7f54617626e1ac44d992cc543d173371a42
Dan McCracken <mccrackend@gmail.com>
2016-03-13 17:49:16 -0400

    trying to break it

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 describe 'test' do
 
   context 'with defaults for all parameters' do
-    it { should contain_class('test') }
+    it { should contain_class('testy testerson') }
   end
 end
