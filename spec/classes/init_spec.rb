aad24a3769ba6c43144dc4af0771e86f744d0a53
Dan McCracken <mccrackend@gmail.com>
2016-03-13 18:26:38 -0400

    fixing test

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 describe 'test' do
 
   context 'with defaults for all parameters' do
-    it { should contain_class('testy testerson') }
+    it { should contain_class('test') }
   end
 end
